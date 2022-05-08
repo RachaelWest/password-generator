4622d07523f75f7a5d8656340816e341dba46587
rachcore1 <78730563+rachcore1@users.noreply.github.com>
2022-05-08 16:00:32 +0100

    updated password generator code

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-const charsArray = ["A","B", "C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","!","@","#","$","%","^","&","*","(",")","_","+","~","}","{","[","]",":",";","?",">","<",".","/","-","="]
+const charsArray = ["A","B", "C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","!","@","#","$","%","^","&","*","(",")","_","+","~","}","{","[","]",":",";","?",">","<",".","/","-","="];
 
-const passwords = document.querySelectorAll(".password-box")
+const passwords = document.querySelectorAll(".password-box");
 const passwordLength = 15; 
 let password; 
 
 //BUTTONS
-const passwordButton = document.getElementById("passwords-btn");
-passwordButton.addEventListener('click', event => {
-    displayPasswords();
-})
+document.getElementById("passwords-btn").addEventListener('click', () => displayPasswords());
 
-const resetButton = document.getElementById("reset-btn");
-resetButton.addEventListener('click', event => {
-    resetPasswords();
-})
+document.getElementById("reset-btn").addEventListener('click',() => resetPasswords());
 
 //FUNCTION TO GENERATE PASSWORDS
  function getPassword(){
@@ -34,8 +28,7 @@ function displayPasswords() {
   
 //FUNCTION TO RESET PASSWORD FIELDS   
 function resetPasswords() {
-    let password;
     passwords.forEach((item) => {
-        item.value = ""
+        item.value = "";
     })
 }
